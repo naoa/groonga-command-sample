8dc3b8da8bbf1c1efd4296af3e8dba09c39f698e
naoa <visionary.incident@gmail.com>
2014-05-22 21:36:35 +0900

    Don't use OUTPUT and VAR macros

--- commands/echo.c
+++ commands/echo.c
@@ -8,46 +8,17 @@
 # define GNUC_UNUSED
 #endif
 
-/* lib/db.h include/groonga/plugin.h */
-#define GRN_PROC_GET_VAR_BY_OFFSET(offset) (grn_plugin_proc_get_var_by_offset(ctx, user_data, offset))
-
-/* lib/proc.c */
-#define VAR GRN_PROC_GET_VAR_BY_OFFSET
-
-/* lib/output.h */
-#define GRN_OUTPUT_ARRAY_OPEN(name,nelements) \
-(grn_ctx_output_array_open(ctx, name, nelements))
-#define GRN_OUTPUT_ARRAY_CLOSE() \
-(grn_ctx_output_array_close(ctx))
-#define GRN_OUTPUT_MAP_OPEN(name,nelements) \
-(grn_ctx_output_map_open(ctx, name, nelements))
-#define GRN_OUTPUT_MAP_CLOSE() \
-(grn_ctx_output_map_close(ctx))
-#define GRN_OUTPUT_INT32(value) \
-(grn_ctx_output_int32(ctx, value))
-#define GRN_OUTPUT_INT64(value) \
-(grn_ctx_output_int64(ctx, value))
-#define GRN_OUTPUT_FLOAT(value) \
-(grn_ctx_output_float(ctx, value))
-#define GRN_OUTPUT_CSTR(value)\
-(grn_ctx_output_cstr(ctx, value))
-#define GRN_OUTPUT_STR(value,value_len)\
-(grn_ctx_output_str(ctx, value, value_len))
-#define GRN_OUTPUT_BOOL(value)\
-(grn_ctx_output_bool(ctx, value))
-#define GRN_OUTPUT_OBJ(obj,format)\
-
 static grn_obj *
 command_echo(grn_ctx *ctx, GNUC_UNUSED int nargs, GNUC_UNUSED grn_obj **args,
                   grn_user_data *user_data)
 {
-  const char *arg_str = GRN_TEXT_VALUE(VAR(0));
-  int arg_length = GRN_TEXT_LEN(VAR(0));
+  const char *arg_str = GRN_TEXT_VALUE(grn_plugin_proc_get_var_by_offset(ctx, user_data, 0));
+  int arg_length = GRN_TEXT_LEN(grn_plugin_proc_get_var_by_offset(ctx, user_data, 0));
 
-  GRN_OUTPUT_ARRAY_OPEN("RESULT", 2);
-  GRN_OUTPUT_CSTR(arg_str);
-  GRN_OUTPUT_INT64(arg_length);
-  GRN_OUTPUT_ARRAY_CLOSE();
+  grn_ctx_output_array_open(ctx, "RESULT", 2);
+  grn_ctx_output_cstr(ctx, arg_str);
+  grn_ctx_output_int64(ctx, arg_length);
+  grn_ctx_output_array_close(ctx);
 
   return NULL;
 }
